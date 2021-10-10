4e17cd2b892555ac15d0d393e1bd6574f5c95555
Sandeep Kumar <Sandeep Kumar@DESKTOP-18LNCUE>
2021-10-10 12:37:04 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld1
+{
+    class Program
+    {
+        /// <summary>
+        /// Defines the helloworld
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
